31788ac38404234810503a3ceaec8e82465c98fb
Paulo Chaves <opaulochaves@gmail.com>
2017-05-28 09:12:24 -0300

    first commit. Using Gradle 3.4

---
+++ src/main/java/com/opaulochaves/springuiando/SpringuiandoApplication.java
@@ -0,0 +1,12 @@
+package com.opaulochaves.springuiando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringuiandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringuiandoApplication.class, args);
+	}
+}
