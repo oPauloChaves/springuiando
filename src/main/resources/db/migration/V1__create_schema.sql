ef2c06601eed1f593388d2ce5c435574e1ed0c60
Paulo Chaves <opaulochaves@gmail.com>
2017-05-28 15:52:43 -0300

    add jpa, flyway and h2

---
+++ src/main/resources/db/migration/V1__create_schema.sql
@@ -0,0 +1,10 @@
+create table Store (
+    id int8 not null,
+    name varchar(255) not null,
+    email varchar(255) not null unique,
+    status varchar(30) not null,
+    primary key (id)
+);
+
+create sequence hibernate_sequence;
+
