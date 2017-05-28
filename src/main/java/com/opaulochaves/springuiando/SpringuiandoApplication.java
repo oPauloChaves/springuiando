ae36feb4fcb068e1f32a45fb0348ddd79ee8053b
Paulo Chaves <opaulochaves@gmail.com>
2017-05-28 16:29:17 -0300

    add bean to initialize datasource

--- src/main/java/com/opaulochaves/springuiando/SpringuiandoApplication.java
+++ src/main/java/com/opaulochaves/springuiando/SpringuiandoApplication.java
@@ -1,12 +1,24 @@
 package com.opaulochaves.springuiando;
 
+import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
 public class SpringuiandoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringuiandoApplication.class, args);
-	}
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringuiandoApplication.class, args);
+    }
 }
