8c01a62b3c8c99bfd5116298672042c3ca373ddb
Paulo Chaves <opaulochaves@gmail.com>
2017-05-28 16:28:00 -0300

    add initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+/* STORES */
+INSERT INTO store(id, version, name, email, status) VALUES(1, 0, 'Brazil Store', 'brazil.store@app.test', 'ACTIVE');
+INSERT INTO store(id, version, name, email, status) VALUES(2, 0, 'Chile Store', 'chile.store@app.test', 'ACTIVE');
+INSERT INTO store(id, version, name, email, status) VALUES(3, 0, 'German Store', 'german.store@app.test', 'INACTIVE');
+INSERT INTO store(id, version, name, email, status) VALUES(4, 0, 'Canada Store', 'canada.store@app.test', 'ACTIVE');
+INSERT INTO store(id, version, name, email, status) VALUES(5, 0, 'Gana Store', 'gana.store@app.test', 'ACTIVE');
