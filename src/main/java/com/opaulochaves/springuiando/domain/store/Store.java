b10965fe3169e9220c4a316411d2cf63232d112d
Paulo Chaves <opaulochaves@gmail.com>
2017-05-28 16:29:47 -0300

    Store jpa mapping

--- src/main/java/com/opaulochaves/springuiando/domain/store/Store.java
+++ src/main/java/com/opaulochaves/springuiando/domain/store/Store.java
@@ -1,25 +1,45 @@
 package com.opaulochaves.springuiando.domain.store;
 
-import lombok.AllArgsConstructor;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  *
  * @author paulo
  */
+@Entity
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class Store {
-    
+public class Store implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
+
+    @Version
+    private Integer version;
+
+    @NotEmpty
     private String name;
-    
+
+    @Column(unique = true)
+    @Email
+    @NotEmpty
     private String email;
-    
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
     private StoreStatus status;
 }
